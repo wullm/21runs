8b483a6e44e64dfec6ffac32bc9eff4b0f02ed3d
wullm <willem.elbers@gmail.com>
2020-12-27 03:52:58 +0000

    Fixed syntax for system command

--- wasserstein_mpi.py
+++ wasserstein_mpi.py
@@ -71,5 +71,5 @@ for pair in pairs[rank::size]:
 				topo_fname1 = generate_fname(outdir1, "small_topology", model1, rank1, seed1, j, nsigstr, "_betti_" + str(dim) +".dat")
 				topo_fname2 = generate_fname(outdir2, "small_topology", model2, rank2, seed2, j, nsigstr, "_betti_" + str(dim) +".dat")
 				os.system("echo -n " + str(seed1) + " " + str(seed2) + " " + str(j) + " " + nsig_only + " " + str(dim) + " >> " + outfile)
-				os.system("/cosma5/data/durham/dc-elbe1/hera/hera/geom_matching/wasserstein/wasserstein_dist " + topo_fname1 + " " + topo_fname2 " >> " + outfile);
+				os.system("/cosma5/data/durham/dc-elbe1/hera/hera/geom_matching/wasserstein/wasserstein_dist " + topo_fname1 + " " + topo_fname2 + " >> " + outfile);
 				time.sleep(0.5)
