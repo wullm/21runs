9c28e68fb794b248b1d4d811e79745542faa9ef3
wullm <willem.elbers@gmail.com>
2020-12-27 03:14:15 +0000

    Better topology data reduction

--- reduce.py
+++ reduce.py
@@ -3,51 +3,58 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-fnames = listdir(".")
+#Prepare filename using a standard format
+def generate_fname(dir, title, model, rank, seed, slice_num, pre_str, ext):
+	fname = dir + "/" + title + "_" + model + "_" + str(rank) + "_" \
+			+ str(seed) + "_slice_" + str(slice_num) + "_" + pre_str + ext
+	return(fname)
 
-topo_data = {};
-seeds = [];
+#The model to be run
+model = "faint"
+outdir = "faint"
 
-#The data to be reduced
-the_slice = 0
-the_noise_lvl = 0
+#The slices and noise levels to be run
+slices = np.arange(0,11)
+noise_levels = np.array([0.0, 0.1, sqrt(0.1), 1.0, sqrt(10.0), 10.0, 1.0])
+signal_levels = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0])
 
-#Load the data by matching all the relevant files in this directory
-for fname in fnames:
-	if not isfile(fname): continue
-	parts = fname.split("_")
-	if (parts[0] == "topology"):
-		noise_lvl = float(np.real_if_close(parts[-1][:-4]));
-		seed = int(parts[4]);
-		slice_ind = int(parts[6]);
-		rank = int(parts[3]);
-		if (the_slice == slice_ind and the_noise_lvl == noise_lvl):
-			print(fname);
-			topo_data[seed] = np.loadtxt(fname, skiprows=2);
-			seeds.append(seed);
+#The seeds to be run
+seeds=np.array([605816, 150650, 278939, 573177, 691674, 773601, 727308, 828958,
+				398521, 642050, 310828, 471108, 16985, 614889, 40760, 814979])
 
-for seed in seeds:
-	#Load the data
-	X = topo_data[seed]
-	#Extract the dimension column
-	dim = X[:,1]
-	#Load the data per dimension
-	b0 = X[dim==0,2:]
-	b1 = X[dim==1,2:]
-	b2 = X[dim==2,2:]
-	#Deselect points at the boundary
-	b0 = b0[np.isinf(b0[:,1])==False]
-	b1 = b1[np.isinf(b1[:,1])==False]
-	b2 = b2[np.isinf(b2[:,1])==False]
-	#Deselect points with zero persistence
-	b0 = b0[(b0[:,1] - b0[:,0])>0]
-	b1 = b1[(b1[:,1] - b1[:,0])>0]
-	b2 = b2[(b2[:,1] - b2[:,0])>0]
-	#Prepare the filenames
-	out_fname_0 = "topology_slice_" + str(the_slice) + "_noise_" + str(the_noise_lvl) + "_b" + str(0) + "/" + str(seed) + ".dat"
-	out_fname_1 = "topology_slice_" + str(the_slice) + "_noise_" + str(the_noise_lvl) + "_b" + str(1) + "/" + str(seed) + ".dat"
-	out_fname_2 = "topology_slice_" + str(the_slice) + "_noise_" + str(the_noise_lvl) + "_b" + str(2) + "/" + str(seed) + ".dat"
-	#Export the data per dimension
-	np.savetxt(out_fname_0, b0)
-	np.savetxt(out_fname_1, b1)
-	np.savetxt(out_fname_2, b2)
+for rank in range(16):
+	seed = seeds[rank]
+	for j in slices:
+		for noise_lvl, signal_lvl in zip(noise_levels, signal_levels):
+			#Format the signal and noise levels into a string
+			nsigstr = "noise_%.1f_signal_%.1f" % (noise_lvl, signal_lvl)
+
+			#Generate input topology filename
+			topo_fname = generate_fname(outdir, "small_topology", model, rank, seed, j, nsigstr, ".dat")
+
+			#Load the data
+			X = np.loadtxt(topo_fname, skiprows=2)
+			#Extract the dimension column
+			dim = X[:,1]
+			#Load the data per dimension
+			b0 = X[dim==0,2:]
+			b1 = X[dim==1,2:]
+			b2 = X[dim==2,2:]
+			#Deselect points at the boundary
+			b0 = b0[np.isinf(b0[:,1])==False]
+			b1 = b1[np.isinf(b1[:,1])==False]
+			b2 = b2[np.isinf(b2[:,1])==False]
+			#Deselect points with zero persistence
+			b0 = b0[(b0[:,1] - b0[:,0])>0]
+			b1 = b1[(b1[:,1] - b1[:,0])>0]
+			b2 = b2[(b2[:,1] - b2[:,0])>0]
+
+			#Prepare the split filenames
+			out_fname_0 = generate_fname(outdir, "small_topology", model, rank, seed, j, nsigstr, "_betti_0.dat")
+			out_fname_1 = generate_fname(outdir, "small_topology", model, rank, seed, j, nsigstr, "_betti_1.dat")
+			out_fname_2 = generate_fname(outdir, "small_topology", model, rank, seed, j, nsigstr, "_betti_2.dat")
+			
+			#Export the data per dimension
+			np.savetxt(out_fname_0, b0)
+			np.savetxt(out_fname_1, b1)
+			np.savetxt(out_fname_2, b2)
